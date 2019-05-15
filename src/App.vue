2e3fe0eb319c43e1496013ae29b814d163af5a92
Daniel Molina <dmolcap@gmail.com>
2019-05-15 15:21:23 +0200

    vuetify plugin installation

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,35 @@
 <template>
-  <div id="app">
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>-->
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>fas fa-external-link-alt</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import Landing from "@/views/Landing.vue";
 
 export default {
-  components: {
-    Landing
+  name: 'App',
+  data () {
+    return {
+      //
+    }
   }
-};
+}
 </script>
-
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 10px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-
-.margintop {
-  margin-top: 30px;
-}
-</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,53 +1,13 @@
 <template>
-  <div class="container">
-    <div>
-      <Navbar/>
-    </div>
-    <div class="main">
-      <div>
-        <router-link to="/workout">
-          <button>Workout(train)</button>
-        </router-link>
-      </div>
-      <div>
-        <router-link to="/addword">
-          <button>Add Exercises(words)</button>
-        </router-link>
-      </div>
-      <div>
-        <router-link to="/lists">
-          <button>Workout Routines(lists)</button>
-        </router-link>
-      </div>
-      <div>
-        <router-link to="/chats">
-          <button>Supplements Shop(chat rooms)</button>
-        </router-link>
-      </div>
-      <div>
-        <router-link to="/games">
-          <button>Competitions(games)</button>
-        </router-link>
-      </div>
-    </div>
-  </div>
+  <HelloWorld />
 </template>
 
 <script>
-// @ is an alias to /src
-import Navbar from "@/views/Navbar.vue";
+  import HelloWorld from '../components/HelloWorld'
 
-export default {
-  name: "home",
-  components: {
-    Navbar
+  export default {
+    components: {
+      HelloWorld
+    }
   }
-};
 </script>
-
-<style scoped>
-button {
-  padding: 15px 35px;
-  margin-bottom: 10px;
-}
-</style>
