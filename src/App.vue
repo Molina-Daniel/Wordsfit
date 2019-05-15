2fe579fed42d3a14c56bf246d17020f08f74fc71
Daniel Molina <dmolcap@gmail.com>
2019-05-15 16:18:01 +0200

    vuetify successfully installed

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,7 @@
         <span class="font-weight-light">MATERIAL DESIGN</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
+      <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
         <span class="mr-2">Latest Release</span>
         <v-icon>fas fa-external-link-alt</v-icon>
       </v-btn>
@@ -23,13 +19,12 @@
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  data () {
+  name: "App",
+  data() {
     return {
       //
-    }
+    };
   }
-}
+};
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,52 @@
 <template>
-  <HelloWorld />
+  <div class="container">
+    <div>
+      <Navbar/>
+    </div>
+    <div class="main">
+      <div>
+        <router-link to="/workout">
+          <button>Workout(train)</button>
+        </router-link>
+      </div>
+      <div>
+        <router-link to="/addword">
+          <button>Add Exercises(words)</button>
+        </router-link>
+      </div>
+      <div>
+        <router-link to="/lists">
+          <button>Workout Routines(lists)</button>
+        </router-link>
+      </div>
+      <div>
+        <router-link to="/chats">
+          <button>Supplements Shop(chat rooms)</button>
+        </router-link>
+      </div>
+      <div>
+        <router-link to="/games">
+          <button>Competitions(games)</button>
+        </router-link>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    components: {
-      HelloWorld
-    }
+// @ is an alias to /src
+import Navbar from "@/views/Navbar.vue";
+export default {
+  name: "home",
+  components: {
+    Navbar
   }
+};
 </script>
+
+<style scoped>
+button {
+  padding: 15px 35px;
+  margin-bottom: 10px;
+}
+</style>
